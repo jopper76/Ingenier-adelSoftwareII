2e0cde6b11790c98b1e81d2e05b4450822edab44
Jopper <115431471+jopper76@users.noreply.github.com>
2026-06-18 12:51:59 -0600

    Add files via upload

---
+++ CrudNet7MVC/Controllers/InicioController.cs
@@ -0,0 +1,154 @@
+using CrudNet7MVC.Datos;
+using CrudNet7MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CrudNet7MVC.Controllers
+{
+    public class InicioController : Controller
+    {
+
+        private readonly ApplicationDbContext _contexto;
+
+        public InicioController(ApplicationDbContext contexto)
+        {
+
+            _contexto = contexto;
+
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _contexto.Contacto.ToListAsync());
+
+        }
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid) 
+            { 
+                _contexto.Contacto.Add(contacto);
+                await  _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Editar(int? id)
+        {
+
+            if (id == null)
+            {
+
+                return NotFound();
+
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null ){
+                
+                return NotFound();
+
+            }
+
+            return View(contacto);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Contacto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Detalle(int? id)
+        {
+
+            if (id == null)
+            {
+
+                return NotFound();
+
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+
+                return NotFound();
+
+            }
+
+            return View(contacto);
+
+        }
+
+        [HttpGet]
+
+        public IActionResult Borrar(int? id)
+        {
+
+            if (id == null)
+            {
+
+                return NotFound();
+
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+
+                return NotFound();
+
+            }
+
+            return View(contacto);
+
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+
+            var contacto = await _contexto.Contacto.FindAsync(id);
+            if (contacto == null)
+            {
+
+                return View();
+
+            }
+
+            //Borrado
+
+            _contexto.Contacto.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudNet7MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+
+
+        }
+
+        public DbSet<Contacto> Contacto { get; set; }
+
+    }
+}
